bafe9ae9247dda60e767db8af0f581095238abfe
Виктор Тузовский <vtuzovskiy@fura.ru>
2022-04-04 16:59:18 +0300

    Added response for chat messages

--- main.py
+++ main.py
@@ -9,19 +9,24 @@ load_dotenv()
 ACCESS_TOKEN = os.getenv('graph_token')
 TEAM_ID = os.getenv('team_id')
 CHANNEL_ID = os.getenv('channel_id')
+CHAT_ID = os.getenv('chat_id')
 
-URL = (f'https://graph.microsoft.com/beta/'
-       f'teams/{TEAM_ID}/channels/{CHANNEL_ID}/messages')
+URL_CHANNEL = (f'https://graph.microsoft.com/beta/'
+               f'teams/{TEAM_ID}/channels/{CHANNEL_ID}/messages')
+
+URL_CHAT = (f'https://graph.microsoft.com/beta/'
+            f'chats/{CHAT_ID}/messages')
 
 headers = {'Authorization': f'Bearer {ACCESS_TOKEN}'}
 
-
 while True:
     try:
-        response = requests.get(URL, headers=headers).json()
-        URL = response.get('@odata.nextLink')
+        # Вставить необходимую ссылку
+        response = requests.get(URL_CHANNEL, headers=headers).json()
+        # Использовать переменную из ссылки выше
+        URL_CHANNEL = response.get('@odata.nextLink')
         value_list = response.get('value')
-        with open('messages.json', 'a', encoding='utf-8') as outfile:
+        with open('<required name>.json', 'a', encoding='utf-8') as outfile:
             for value in value_list:
                 json.dump(value, outfile, ensure_ascii=False, indent=2)
     except Exception as error:
